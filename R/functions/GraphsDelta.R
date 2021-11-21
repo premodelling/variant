4ca4cfcc573caf1860a86937bf7458f78c645bbb
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-11-21 01:56:55 +0000

    Update R/functions/GraphsDelta.R

--- R/functions/GraphsDelta.R
+++ R/functions/GraphsDelta.R
@@ -12,7 +12,8 @@ GraphsDelta <- function (local) {
     geom_line(size = 0.15) +
     geom_point(size = 0.75) +
     theme_minimal()  +
-    theme(axis.text.x = element_text(size = 11, angle = 90),
+    theme(plot.title = element_text(size=15, face = 'bold', hjust = 0.5),
+          axis.text.x = element_text(size = 11, angle = 90),
           axis.text.y = element_text(size = 11),
           axis.title.x = element_text(face = 'bold', size = 13),
           axis.title.y = element_text(face = 'bold', size = 13),
@@ -20,6 +21,7 @@ GraphsDelta <- function (local) {
           panel.grid.minor = element_blank(), panel.grid.major = element_line(size = 0.15)) +
     xlab('\n') +
     ylab('Delta Variant Proportion\n') +
-    scale_x_date(date_breaks = "1 month", date_labels =  "%b %Y")
+    ggtitle(label = '\nDelta Variant Infections Over Time\n') +
+    scale_x_date(date_breaks = "4 weeks", date_labels =  "%d %b %Y")
 
 }
