6cad98fbb80ac6c54993e99d6e0548df060d13d0
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-11-28 00:10:52 +0000

    Update R/functions/VariantProgressionModel.R

--- R/functions/VariantProgressionModel.R
+++ R/functions/VariantProgressionModel.R
@@ -1,20 +1,25 @@
-# Title     : TODO
-# Objective : TODO
+# Title     : Variant Progression Model
+# Objective : Predict fractional contributions per day
 # Created by: Think
 # Created on: 20/11/2021
 
-
+#' @param starting: The predictions calculation start date; class Date.
+#' @param period: The length of time - from 'starting' onwards -  for which predictions are calculated.  The
+#'                value must be a lubridate::years(), lubridate::weeks(), or lubridate::days() value
 VariantProgressionModel <- function (starting, period) {
 
+  # inspection
   if ( !( period %in% c(lubridate::years(), lubridate::weeks(), lubridate::days()) ) ) {
     stop('The calculation period must be a lubridate::years(), lubridate::weeks(), or lubridate::days() period')
   }
 
 
-  # ... model
+  # ... the model parameters
   beta <- 0.0277
   alpha <- -526.0385
 
+
+  # ... the prediction equation
   p <- function (d) {
     return(exp(x = d * beta + alpha))
   }
