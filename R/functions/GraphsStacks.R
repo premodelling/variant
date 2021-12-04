7884dbed3ef99d41139191796bbc0882dfbe8458
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-12-04 01:04:37 +0000

    Update R/functions/GraphsStacks.R

--- R/functions/GraphsStacks.R
+++ R/functions/GraphsStacks.R
@@ -11,9 +11,15 @@ StacksTravelYes <- function (stacksdata) {
     ggplot(mapping = aes(x = week, y = proportion), alpha = 0.25) +
     geom_col(aes(fill = segment), alpha = 0.60) +
     theme_minimal() +
+    theme(axis.text.x = element_text(size = 11, angle = 90),
+          axis.text.y = element_text(size = 11),
+          axis.title.x = element_text(face = 'bold', size = 13),
+          axis.title.y = element_text(face = 'bold', size = 13),
+          legend.title = element_text(),
+          panel.grid.minor = element_blank(), panel.grid.major = element_line(size = 0.15)) +
     xlab('\n') +
     ylab('Variant Proportion\n') +
-    scale_x_date(date_breaks = "8 weeks", date_labels =  "%d %b %Y") +
+    scale_x_date(date_breaks = "8 weeks", date_labels =  "%b %Y") +
     scale_fill_discrete(name = 'Variant')
 
 }
@@ -25,9 +31,15 @@ StacksTravelNo <- function (stacksdata) {
     ggplot(mapping = aes(x = week, y = proportion), alpha = 0.25) +
     geom_col(aes(fill = segment), alpha = 0.60) +
     theme_minimal() +
+    theme(axis.text.x = element_text(size = 11, angle = 90),
+          axis.text.y = element_text(size = 11),
+          axis.title.x = element_text(face = 'bold', size = 13),
+          axis.title.y = element_text(face = 'bold', size = 13),
+          legend.title = element_text(),
+          panel.grid.minor = element_blank(), panel.grid.major = element_line(size = 0.15)) +
     xlab('\n') +
     ylab('Variant Proportion\n') +
-    scale_x_date(date_breaks = "8 weeks", date_labels =  "%d %b %Y") +
+    scale_x_date(date_breaks = "8 weeks", date_labels =  "%b %Y") +
     scale_fill_discrete(name = 'Variant')
 
 }
