27b61d838bc1920f5b42a3e7da206dfd512aebaf
greyhypotheses <10231132+greyhypotheses@users.noreply.github.com>
2021-12-04 02:51:27 +0000

    Update R/functions/GraphsVUI21OCT01.R

--- R/functions/GraphsVUI21OCT01.R
+++ R/functions/GraphsVUI21OCT01.R
@@ -26,14 +26,32 @@ ProgressionVUI21OCT01 <- function (vui21oct01) {
 #' @param vui21oct01
 PredictionsVUI21OCT01 <- function (vui21oct01, estimates) {
 
-  plot(x = vui21oct01$week, y = vui21oct01$VUI.21OCT.01,
-       ylim=c(min(vui21oct01$VUI.21OCT.01, estimates$prediction), max(vui21oct01$VUI.21OCT.01, estimates$prediction)),
-       xlim = c(min(vui21oct01$week, estimates$date), max(vui21oct01$week, estimates$date)),
-       type = "l", lty = 'solid', col = 'black', lwd = 2.5,
-       frame.plot = FALSE,
-       xlab = '', ylab = 'fraction', main = 'VUI.21OCT.01 Infection Fractions Over Time\n(non-travel)')
-  points(x = estimates$date, y = estimates$prediction,
-         pch = 19, cex = 0.5,
-         col = scales::alpha(colour = 'green', alpha = 0.25))
+  T <- vui21oct01 %>%
+    mutate(series = 'original')
+  names(T) <- c('date', 'fraction', 'series')
+
+  P <- estimates %>%
+    select(date, prediction) %>%
+    mutate(series = 'prediction')
+  names(P) <- c('date', 'fraction', 'series')
+
+  timeseries <- rbind(T, P)
+  timeseries$series <- factor(timeseries$series, levels = c('original', 'prediction'))
+
+  ggplot(data = timeseries, mapping = aes(x = date, y = fraction, colour = series)) +
+    geom_line(alpha = 0.55) +
+    geom_point(alpha = 0.15, size = 1) +
+    theme_minimal() +
+    theme(axis.text.x = element_text(size = 11, angle = 90),
+          axis.text.y = element_text(size = 11),
+          axis.title.x = element_text(face = 'bold', size = 13),
+          axis.title.y = element_text(face = 'bold', size = 13),
+          legend.title = element_text(),
+          panel.grid.minor = element_blank(), panel.grid.major = element_line(size = 0.15)) +
+    xlab('\n') +
+    ylab('variant fraction\n') +
+    scale_x_date(date_breaks = "2 months", date_labels =  "%b %Y") +
+    scale_colour_manual(values = c('black', 'orange')) +
+    guides(col = guide_legend(title = 'VUI.21OCT.01\nFractions'))
 
 }
